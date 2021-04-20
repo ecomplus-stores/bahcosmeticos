bcdb6a054fad602b66274955c50182a74ab8bdc6
eduvlemes <edu.vlemes@gmail.com>
2021-04-20 16:09:41 -0300

    Ajuste marcas e botão de cor no produto

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,8 @@ console.log(path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'
 module.exports = () => ({
   resolve: {
     alias: {
-      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html')
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
+      './html/ProductVariations.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductVariations.html')
     }
   }
 })
